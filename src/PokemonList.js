9b0096e33b8c000d7cc95ff81094490d1a2cd25d
Delaney Fogarty <delaney.fk@gmail.com>
2022-05-17 17:27:27 -0700

    working on pokemon search working

---
+++ src/PokemonList.js
@@ -0,0 +1,14 @@
+export default function PokemonList({ poke }) {
+  return (
+    <div className="pokemon">
+      {poke.map(({ pokemon, attack, defense, url_image }, i) => (
+        <div key={pokemon + i}>
+          <h2>{pokemon}</h2>
+          <p>Attack: {attack}</p>
+          <p>Defense: {defense}</p>
+          <img src={url_image} />
+        </div>
+      ))}
+    </div>
+  );
+}
