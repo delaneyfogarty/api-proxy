7f003a7b79dafe898248e4c2f878a31a281d6358
Delaney Fogarty <delaney.fk@gmail.com>
2022-05-17 17:41:36 -0700

    commiting as I go

--- src/PokemonSearch.js
+++ src/PokemonSearch.js
@@ -4,7 +4,7 @@ import PokemonList from './PokemonList';
 
 export default function PokemonSearch() {
   // you'll need to track your pokemon search results, the loading state, and one form field: name. For this form field, set a real initial values (like 'pikachu') so the form populates with a default value.
-  const [pokemon, setPokemon] = useState([]);
+  const [poke, setPokemon] = useState([]);
   const [pokeQuery, setPokeQuery] = useState('pikachu');
   const [isLoading, setIsLoading] = useState(false);
 
@@ -13,11 +13,12 @@ export default function PokemonSearch() {
     setIsLoading(true);
     // set the loading state to true
     // use fetch to make a request to your netlify pokemon function. Be sure to pass the pokemon name as a query param in the URL
-    const netlifyRequest = await fetch(`/.netlify/functions/pokemon?query=${pokemon}`);
+    const netlifyRequest = await fetch(`/.netlify/functions/pokemon?pokemon=${poke}`);
 
     const data = await netlifyRequest.json();
+
     setIsLoading(false);
-    setPokemon(data);
+    setPokemon(data.data);
     // put the jsonified data in state and set the loading state to false
   }
 
@@ -25,12 +26,12 @@ export default function PokemonSearch() {
     <section className="pokemon">
       {/* make the fetch on submit */}
       <form onSubmit={handlePokemonSubmit}>
-        Search pokemon for a city
+        Search pokemon
         <input value={pokeQuery} onChange={(e) => setPokeQuery(e.target.value)} />
         {/* add inputs/labels for the pokemon name, using all the things we need with react forms. Don't forget to use the value property to sync this up with the default value in react state */}
         <button>Get pokemon</button>
       </form>
-      <div>{isLoading ? <Spinner /> : <PokemonList poke={pokemon} />}</div>
+      <header>{isLoading ? <Spinner /> : <PokemonList poke={poke} />}</header>
       {/* Make a PokemonList component to import and use here. Use a ternery to display a loading spinner (make a <Spinner /> component for this) if the data is still loading. */}
     </section>
   );
